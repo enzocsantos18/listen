59921a309c935413e7fe787b161154bce3b61958
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-06-10 14:31:00 -0300

    Splash Screen and Icon

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import * as Speech from 'expo-speech';
 import logo from '../../assets/logo.png'
 import { Container, Logo, Input, ListenButton, ListenText} from './styles';
 
 const Home = () => {
+
+  const [text, setText] = useState('');
+
+  function handleListen(){
+    Speech.speak(text, {language: 'pt-BR'})
+  }
+
   return (
     <Container>
       <Logo source={logo} />
       <Input 
-      style={{ textAlignVertical : "top" }}
+        value={text}
+        onChangeText={text => setText(text)}
+        style={{ textAlignVertical : "top" }}
         scrollEnabled={true}
         multiline={true}
         placeholder="Digite um texto aqui..."
       />
-      <ListenButton>
+      <ListenButton onPress={handleListen}>
         <ListenText>ESCUTAR</ListenText>
       </ListenButton>
     </Container>
